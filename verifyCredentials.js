daf939b92fc213a9e1f49d4a22dc26aa081bdeb8
williamnwogbo <williamnwogbo@gmail.com>
2019-12-20 16:16:00 +0100

    =...

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -6,19 +6,19 @@ module.exports = async function verify(credentials, cb) {
     // if (!credentials.username || credentials.password) {
     //     return cb(null, { verified: false });
     // }
-    debug('To verify credentials send request to %s', url);
-
-    var options = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true, };
-
-    const uri = "mongodb+srv://"+credentials.username+":"+credentials.password+"@sync-thatapp-mrjt2.gcp.mongodb.net/test?retryWrites=true&w=majority";
-console.log(uri);
-    await MongoClient.connect(uri, options,function(err, client) {
-        assert.equal(null, err);
-        console.log("Connected successfully to server");
-
-    });
+//     debug('To verify credentials send request to %s', url);
+//
+//     var options = {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true, };
+//
+//     const uri = "mongodb+srv://"+credentials.username+":"+credentials.password+"@sync-thatapp-mrjt2.gcp.mongodb.net/test?retryWrites=true&w=majority";
+// console.log(uri);
+//     await MongoClient.connect(uri, options,function(err, client) {
+//         assert.equal(null, err);
+//         console.log("Connected successfully to server");
+//
+//     });
     return cb(null, { verified: true });
 };
 
