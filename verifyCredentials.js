61817280eaf7e7bd40e850be105fe486e4d3731d
williamnwogbo <williamnwogbo@gmail.com>
2019-12-20 15:54:08 +0100

    =...

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -6,13 +6,14 @@ module.exports = async function verify(credentials, cb) {
     if (!credentials.username || credentials.password) {
         return cb(null, { verified: false });
     }
+    debug('To verify credentials send request to %s', url);
 
     var options = {
         useNewUrlParser: true,
         useUnifiedTopology: true, };
 
     const uri = "mongodb+srv://"+credentials.username+":"+credentials.password+"@sync-thatapp-mrjt2.gcp.mongodb.net/test?retryWrites=true&w=majority";
-
+console.log(uri);
     await MongoClient.connect(uri, options,function(err, client) {
         assert.equal(null, err);
         console.log("Connected successfully to server");
