17b17c18dccb16debc87545e6ed6a2cbbd61b940
williamnwogbo <williamnwogbo@gmail.com>
2019-12-20 16:38:38 +0100

    =...

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-module.exports =  function verify(credentials, cb) {
+module.exports =  async function verify(credentials, cb) {
     console.log(JSON.stringify(credentials));
 
     var options = {
@@ -11,10 +11,11 @@ module.exports =  function verify(credentials, cb) {
     const uri = "mongodb+srv://"+credentials.username+":"+credentials.password+"@sync-thatapp-mrjt2.gcp.mongodb.net/test?retryWrites=true&w=majority";
     console.log(uri);
 
-    // await MongoClient.connect(uri, options,function(err, client) {
-    //     console.log(err);
-    //     console.log("Connected successfully to server");
-    // });
+    await MongoClient.connect(uri, options,function(err, client) {
+        console.log(err);
+        console.log("Connected successfully to server");
+    });
+
     return cb(null, { verified: true });
 };
 
