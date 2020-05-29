daf246a535e900f0342fad1b6daebff8c1421381
williamnwogbo <williamnwogbo@gmail.com>
2020-05-29 20:03:11 +0100

    =Debugging

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,14 +1,22 @@
-const MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient,
+    f = require('util').format,
+    fs = require('fs');
+
 const assert = require('assert');
+
+var ca = [fs.readFileSync(__dirname + "/lib/ssl/ca.pem")];
 
 module.exports =  async function verify(credentials, cb) {
     console.log(JSON.stringify(credentials));
 
     var options = {
         useNewUrlParser: true,
-        useUnifiedTopology: true, };
+        useUnifiedTopology: true,
+        sslValidate:true,
+        sslCA:ca
+    };
 
-    const uri = "mongodb:+srv://"+${credentials.username}+":"+${credentials.password}+"@mongo-0.kubernetes.thatapp.io,mongo-1.kubernetes.thatapp.io,mongo-2.kubernetes.thatapp.io:30127/?authSource=admin&replicaSet=mongodb-replicaset-operator&readPreference=secondaryPreferred&appname=MongoDB%20Compass&ssl=true";
+    const uri ="mongodb://" + ${credentials.username} + ":" + ${credentials.password} + "@mongo-0.kubernetes.thatapp.io,mongo-1.kubernetes.thatapp.io,mongo-2.kubernetes.thatapp.io:27017/?authSource="+${credentials.db_admin}+"&replicaSet=mongodb-replicaset-operator&readPreference=secondaryPreferred&ssl=true";
     console.log(uri);
 
     await MongoClient.connect(uri, options,function(err, client) {
