933602c92e7d6d80a22928482281134fce39b638
williamnwogbo <williamnwogbo@gmail.com>
2020-11-03 16:57:13 +0100

    =Change Mongo DB

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -4,17 +4,16 @@ var MongoClient = require('mongodb').MongoClient,
 
 const assert = require('assert');
 
-var ca = [fs.readFileSync(__dirname + "/lib/ssl/ca.pem")];
 
 module.exports =  async function verify(credentials, cb) {
     console.log(JSON.stringify(credentials));
 
     var options = {
         useNewUrlParser: true,
+        useUnifiedTopology: true
     };
 
     const uri = "mongodb+srv://" + username + ":" + password +"@migratecluster0.si2d6.mongodb.net/admin?connectTimeoutMS=300000&keepAlive=300000";
-    console.log(uri);
 
     await MongoClient.connect(uri, options,function(err, client) {
         console.log(err);
