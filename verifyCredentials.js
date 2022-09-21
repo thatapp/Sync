c43be2b12a86dcc8cc3a624679ff1d336a911989
if0s <if0s.3d@gmail.com>
2022-09-21 15:01:51 +0300

    retries, tests, metadata, documentation

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,17 +1,16 @@
-const { Client } = require("./lib/client");
+const { Client } = require('./lib/client');
 
 module.exports = async function verify(cfg) {
-    try {
-        const client = new Client(this, cfg);
-        const filter = { item_id: "8293825" };
+  try {
+    const client = new Client(this, cfg);
+    const filter = { item_id: '8293825' };
 
-        await client.apiRequest('findOne', { filter }, { body: { database: "avaSync_DB", collection: "sync" } });
+    await client.apiRequest('findOne', { filter }, { body: { database: 'avaSync_DB', collection: 'sync' } });
 
-        this.logger.info('Verification completed successfully');
-        return { verified: true };
-    } catch (e) {
-        this.logger.error('Verification failed');
-        throw e;
-    }
+    this.logger.info('Verification completed successfully');
+    return { verified: true };
+  } catch (e) {
+    this.logger.error('Verification failed');
+    throw e;
+  }
 };
-
